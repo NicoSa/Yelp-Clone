562f7c993a59c9cba8f27b3ea5d3fd5c8cbc1ac1
Nico Saueressig <nico@nicosaueressig.de>
2014-05-19 14:28:45 +0100

    added basic adding restaurants function, displays as well

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,17 @@
 class RestaurantsController < ApplicationController
+
+
+	def new
+		@restaurant = Restaurant.new
+	end
+
+	def index
+		@restaurants = Restaurant.all
+	end
+
+	def create
+		Restaurant.create({name: 'InAndOutBurger', address: '1 California Drive, Los Angeles'})
+		redirect_to('/restaurants')
+	end
+
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
 describe 'restaurants index page' do
-  
+
   context 'no restaurants have been added' do
     it 'should display a message' do
       visit('/restaurants')
       expect(page).to have_content 'No restaurants yet'
+    end
+  end
+
+  context 'creating a restaurant' do
+    it 'adds it to the restaurants index' do
+      visit ('/restaurants/new')
+      fill_in 'Name', with: 'InAndOutBurger'
+      fill_in 'Address', with: '1 California Drive, Los Angeles'
+      click_button 'Create Restaurant'
+
+      expect(current_path).to eq '/restaurants'
+      expect(page).to have_content 'InAndOutBurger'
     end
   end
 
