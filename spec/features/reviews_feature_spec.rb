6de45a47883649acd657e3b3d5111236c6577f5c
Nico Saueressig <nico@nicosaueressig.de>
2014-05-20 15:48:14 +0100

    added test for adding reviews

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
 describe 'reviews function' do
+  
   before { Restaurant.create(name: 'KFC', address: '1 high st, London', cuisine: 'Chicken') }
 
   specify 'no reviews have been added' do
     visit('/restaurants')
     expect(page).to have_content 'No reviews'
+  end
+
+  it 'one review has been added' do
+  	visit('/restaurants')
+  	click_on('Add Review')
+  	fill_in :comment, with: 'Very good, awesome!'
+	select('5', :from => 'Rating')
+	click_on('Submit Review')
+	expect(current_path).to eq '/restaurants'
+	expect(page).to have_content('Very good, awesome')
+	expect(page).to have_content('5')
   end
 
   
